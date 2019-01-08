5eb702e598a15c1bf0fceb302299cd36e269e79a
18001939 <18001939@e003m05.istic.univ-rennes1.fr>
2019-01-08 10:06:46 +0100

    Commit initial

---
+++ src/main/java/tp1/test/App.java
@@ -0,0 +1,72 @@
+package tp1.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        cancaner();
+    }
+    
+    public static void cancaner() {
+    	System.out.println("COIN");
+    	boolean b = true;
+    	char c = 'c';
+    	int nb = 0;
+    	b = true;
+		nb++;
+		System.out.println(nb);
+    	b = true;
+		nb++;
+		System.out.println(nb);
+    	b = true;
+		nb++;
+		System.out.println(nb);
+    	b = true;
+		nb++;
+		System.out.println(nb);
+    	b = true;
+		nb++;
+		System.out.println(nb);
+		System.out.println(nb);
+    	if (b) {
+			System.out.println("COINCOIN");
+			b = true;
+			nb = 1;
+		}
+    	if (b) {
+			System.out.println("COINCOIN");
+			b = true;
+			nb = 1;
+		}
+    }
+    
+    public void manger() {
+    	if (true) {
+    		if (true) {
+				if (true) {
+					if (true) {
+						if (true) {
+							
+						} else {
+
+						}
+					} else {
+
+					}
+				} else {
+
+				}
+			} else {
+
+			}
+		}
+    	else {
+
+		}
+    }
+}
